c21b5a90c0379c8770604f64b9f6ee8f711ba12c
Steve Pederson <stephen.pederson@adelaide.edu.au>
2019-01-16 17:03:30 +1030

    Set kallisto running

--- bash/runKallisto.sh
+++ bash/runKallisto.sh
@@ -1,51 +1,30 @@
 #!/bin/bash
 #SBATCH -p batch
 #SBATCH -N 1
-#SBATCH -n 16
-#SBATCH --time=24:00:00
-#SBATCH --mem=32GB
+#SBATCH -n 1
+#SBATCH --time=00:02:00
+#SBATCH --mem=1GB
 #SBATCH -o /data/biohub/20181113_MorganLardelli_mRNASeq/slurm/%x_%j.out
 #SBATCH -e /data/biohub/20181113_MorganLardelli_mRNASeq/slurm/%x_%j.err
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=stephen.pederson@adelaide.edu.au
 
-## Cores
-CORES=16
-
-## Modules
-module load kallisto/0.43.1-foss-2017a
-module load SAMtools/1.3.1-GCC-5.3.0-binutils-2.25
-
-## Reference Files
-IDX=/data/biorefs/reference_genomes/ensembl-release-94/danio-rerio/kallisto/Danio_rerio.GRCz11.cdna.inc.psen2mutants
-GTF=/data/biorefs/reference_genomes/ensembl-release-94/danio-rerio/Danio_rerio.GRCz11.94.inc.psen2mutants.gtf
-
 ## Directories
 PROJROOT=/data/biohub/20181113_MorganLardelli_mRNASeq
 TRIMDATA=${PROJROOT}/1_trimmedData
 
 ## Setup for kallisto output
-ALIGNDATA=${PROJROOT}/3_kallisto
-mkdir -p ${ALIGNDATA}
+mkdir -p ${PROJROOT}/3_kallisto
 
-##--------------------------------------------------------------------------------------------##
-## Aligning trimmed data to the kallisto index
-##--------------------------------------------------------------------------------------------##
+## Fire off the alignments
+FQ=$(ls ${TRIMDATA}/fastq/*R1.fq.gz | egrep '[0-9]+_(FS|FAD|_-)_[0-9]_R1.fq.gz')
+echo -e "Found:\n\t${FQ}"
 
-## Aligning, filtering and sorting
-R1=(ls ${TRIMDATA}/fastq/*R1.fq.gz | egrep '[0-9]+_(FS|FAD|_-)_[0-9]_R1.fq.gz')
-echo -e "Found:\n\t${R1}"
-
-exit
-
-kallisto quant \
-	-t ${CORES} \
-	--pseudobam \
-	--single \
-	--fr-stranded \
-	-i ${IDX} \
-	-o ${ALIGNDATA}
+for F1 in ${FQ}
+	do 
+	sbatch ${PROJROOT}/bash/kallisto.sh ${F1}
+done
 
 
 
